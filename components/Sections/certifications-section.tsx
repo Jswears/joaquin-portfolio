a3127ea9a222ebb661163d132c6a9a0a1e0d273c
Jswears <ji.swearssalinas@gmail.com>
2025-02-24 18:34:59 +0100

    adding badge

--- components/Sections/certifications-section.tsx
+++ components/Sections/certifications-section.tsx
@@ -7,7 +7,7 @@ const CertificationsSection = () => {
         <section id="certifications" className="py-20 bg-dark-charcoal">
             <div className="container mx-auto px-4">
                 <SectionTitle number={3} title="Certifications" />
-                <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-6 ">
                     {certifications.map((cert) => (
                         <CertificationCard key={cert.name} {...cert} />
                     ))}
